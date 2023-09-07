560a89ec22541a92bb4419ecc868d9dc15578760
Mariia_Denisova <mariia.denisova@ihsmarkit.com>
2023-09-08 03:05:00 +0600

    updated activity monitor functionality

--- activityMonitor.js
+++ activityMonitor.js
@@ -1,38 +1,58 @@
 const { EventEmitter } = require('events');
 const fs = require('fs');
+const os = require('os');
+const { exec } = require('node:child_process');
 
-const UPDATE_INTERVAL = 100;
+const UPDATE_INTERVAL = 7500; // 8 times per minute
 const MAX_DURATION = 100000000;
 const ONE_MINUTE_IN_MILLISECONDS = 60000;
-function updateActivityLog() {
-    console.log('updateActivityLog function called');
-    fs.appendFile('activityMonitor.log', '<unixtime> : <process info>'+ '\n', (err) => {
+const CARRIAGE_RETURN = '\r';
+
+const OS_TYPES = {
+    LINUX: 'Linux',
+    MACOS: 'Darwin',
+    WINDOWS: 'Windows_NT',
+}
+
+const PROCESS_COMMAND = {
+    [OS_TYPES.WINDOWS]: 'powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + \' \' + $_.CPU + \' \' + $_.WorkingSet }"',
+    [OS_TYPES.MACOS]: 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1',
+    [OS_TYPES.LINUX]: 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1',
+}
+
+function getSystemDetails(command, callback) {
+    exec(command)
+        .stdout
+        .on('data', (data) => {
+            callback(data);
+            process.stdout.write(data)
+        })
+}
+
+function updateActivityLog(processInfo) {
+    console.log('processInfo', processInfo);
+    fs.appendFile('activityMonitor.log', `<${Date.now()}> : <${processInfo}\n>`, (err) => {
         if (err) throw err;
-        console.log('Saved!');
     });
 }
 
 function activityMonitor() {
-    console.log('activityMonitor function called');
+    const operationSystem = os.type();
     const emitter = new EventEmitter();
     let timeCounter = 0;
+    let processInfo = '';
+
+    const updateProcessInfo = (newInfo) => {
+        processInfo = newInfo;
+    }
 
     const interval = setInterval(() => {
         timeCounter = timeCounter + UPDATE_INTERVAL;
-        console.log('activityMonitor works', timeCounter);
-
-        // TODO: somehow handle errors
+        getSystemDetails(PROCESS_COMMAND[operationSystem], updateProcessInfo);
 
         // updates activity log every minute
         if (timeCounter % ONE_MINUTE_IN_MILLISECONDS === 0) {
-            updateActivityLog();
-            console.log('timeCounter', timeCounter)
-        }
-
-        // finishes function
-        if (timeCounter === MAX_DURATION) {
-            clearInterval(interval);
-            emitter.emit('end');
+            updateActivityLog(processInfo);
         }
     }, UPDATE_INTERVAL);
 
@@ -40,11 +60,3 @@ function activityMonitor() {
 }
 
 const monitor = activityMonitor();
-
-monitor.on('success', (count) => {
-    console.log(`Count is: ${count}`);
-});
-
-monitor.on('end', () => {
-    console.info('Counter has ended');
-});
