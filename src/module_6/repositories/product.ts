4dd6d401c95783ba707853527859b80bbd271fc1
Mariia_Denisova <mariia.denisova@ihsmarkit.com>
2023-10-31 05:46:31 +0600

    fixed getProductsList function

--- src/module_6/repositories/product.ts
+++ src/module_6/repositories/product.ts
@@ -1,8 +1,8 @@
 import { ProductModel } from '../../module_7/entities/types';
 import Product from '../../module_7/entities/product';
 
-const getProductsList = async (): Promise<ProductModel | null> => {
-    return await Product.findOne({}).exec();
+const getProductsList = async (): Promise<ProductModel[] | null> => {
+    return await Product.find({}).exec();
 };
 const getProductById = async (productId: string): Promise<ProductModel | null> => {
     const product = await Product.findOne({ _id: productId }).exec();
